670435a26e72250a9464facedda31f1cd594d1f7
Tom Betka <tcbetka@hotmail.com>
2021-03-25 19:31:02 -0500

    Initial commit for project.

---
+++ Ep002/source/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <concepts>
+
+using std::cout;
+using std::endl;
+
+template <typename T>
+requires std::integral<T>
+T add(T a, T b) 
+{
+    return a + b;
+}
+
+int main() 
+{
+    cout << "Hello from C++20 with CMake on Linux" << endl;
+    cout << "7 + 5 = " << add(7, 5) << endl; 
+
+    return 0;
+}
