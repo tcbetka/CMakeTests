bc4547079559d7c41849734969a34820bbdd7a3a
Tom Betka <tcbetka@hotmail.com>
2021-03-30 15:07:16 -0500

    Function pointers, basic syntax

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -2,47 +2,31 @@
 using std::cout;
 using std::endl;
 
-// Can have class templates
-template<typename T, typename U>
-class Test
+void test() 
 {
-private:
-    T mObj1;
-    U mObj2;
-
-public:
-    Test(T obj1, U obj2) : mObj1(obj1), mObj2(obj2) {}
-
-    void print() const {
-        cout << "mObj1: " << mObj1 << "\tmObj2: " << mObj2 << endl;
-    }
-};
-
-// Can also have function templates
-template<typename T>
-void print(T arg) {
-    cout << "arg: " << arg << endl;
+    cout << "Hello" << endl;
 }
 
+void test2(int a)
+{
+    cout << "a: " << a << endl;
+}
 
 int main()
 {
-    Test<int, double> test1(1, 10.001);
-    test1.print();
+    // Declare a function pointer, and point it to the test function
+    void (*fPtr)();
+    fPtr = test;
 
-    Test<double, char> test2(3.14159, 'a');
-    test2.print();
+    // Now we can call the test() function via the function pointer
+    fPtr();
 
-    Test<std::string, std::string> test3("Hello", "World");
-    test3.print();
+    // Or we can also dereference the function pointer and use that to call the function
+    (*fPtr)();
 
-    // Use our template function, explicitly indicating the template type
-    print<int>(1);
-    print<double>(3.14);
-
-    // Or...the compiler will even infer the type for us
-    print(4.56);
-    print("Hello");
+    // We can have function pointers to functions that take parameters as well 
+    void (*fPtr2)(int) = test2;
+    fPtr2(10);
 
     return 0;
 }
