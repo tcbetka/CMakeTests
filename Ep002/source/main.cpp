703fc6a3f1f23a37d1f0de1597f46b9a293233c1
Tom Betka <tcbetka@hotmail.com>
2021-03-30 16:02:42 -0500

    Abstract class example code.

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -5,41 +5,52 @@ using std::endl;
 #include <vector>
 using std::vector;
 
-bool match(std::string test)
+// Can make a base class uninstantiable by creating at least one pure virtual method
+class Animal
 {
-    // Check to see if the string size is 3 chars
-    return test.size() == 3;
-}
+public: 
+    // This is pure virtual, making the entire class "abstract" and therefore uninstantiable
+    //  So now any class that wishes to derive from Animal MUST provide an implementation for
+    //  the pure virtual function(s) in the Animal class
+    virtual void speak() = 0;
+    virtual void run() = 0;
+};
 
-int countStrings(vector<std::string>& texts, bool (*myMatchFunc)(std::string test)) 
+class Dog : public Animal
 {
-    int count = 0;
-    for (vector<std::string>::iterator it = texts.begin(); it != texts.end(); it++) {
-        if (myMatchFunc(*it)) {
-            count++;
-        }
-    }
+public:
+    // Must implement all pure virtual methods in its base class, or it will itself 
+    //  be an abstract class
+    virtual void speak() override { cout << "Woof!" << endl; }
 
-    return count;
-}
+    // Because we haven't provided an implementation for the Animal::run() method, 
+    //  the Dog class is still abstract, and as such cannot be instantiated
+};
+
+class Labrador : public Dog 
+{
+public:
+    virtual void run() override { cout << "Running!" << endl; }
+};
 
 int main()
 {
-    vector<std::string> texts;
-    texts.push_back("one");
-    texts.push_back("two");
-    texts.push_back("three");
-    texts.push_back("four");
-    texts.push_back("five");
-    texts.push_back("size");
-    texts.push_back("seven");
+    Labrador d;
+    d.speak();  // From Dog class
+    d.run();    // From Labrador class
+    cout << endl;
 
-    // Now we can search the vector and find the number of strings with three chars
-    int number = std::count_if(texts.begin(), texts.end(), match);
-    cout << "Number: " << number << endl; 
+    // Cannot do this, because Animal class is abstract and this line will 
+    //  cause the Animal ctor to run 5 times...which cannot happen as Animal
+    //  is an abstract class. 
+    //      Animal animals[5];
 
-    // Use our version 
-    cout << "Count of strings: " << countStrings(texts, match) << endl;
+    // We can however create an array of Animal pointers, however when we loaded
+    //  the array we'd need to use concrete (ie; non-virtual) derived class objs
+    Animal* animals[5];
+    animals[0] = &d;
+    animals[0]->speak();
+    animals[0]->run();
 
     return 0;
 }
