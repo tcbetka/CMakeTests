295567bd959ab190ee20bb362a46653da07dc020
Tom Betka <tcbetka@hotmail.com>
2021-03-30 16:23:59 -0500

    Added functor example.

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -5,52 +5,41 @@ using std::endl;
 #include <vector>
 using std::vector;
 
-// Can make a base class uninstantiable by creating at least one pure virtual method
-class Animal
+// Functors can be either classes or structs, and we can implement an inheritance hierarchy
+//  just like with other C++ constructs
+struct Test
 {
-public: 
-    // This is pure virtual, making the entire class "abstract" and therefore uninstantiable
-    //  So now any class that wishes to derive from Animal MUST provide an implementation for
-    //  the pure virtual function(s) in the Animal class
-    virtual void speak() = 0;
-    virtual void run() = 0;
+    // Make this pure virtual
+    virtual bool operator()(std::string& test) = 0;
 };
 
-class Dog : public Animal
+// Now MatchTest MUST implement the operator() overload in order to be instantiable
+struct MatchTest : public Test 
 {
-public:
-    // Must implement all pure virtual methods in its base class, or it will itself 
-    //  be an abstract class
-    virtual void speak() override { cout << "Woof!" << endl; }
-
-    // Because we haven't provided an implementation for the Animal::run() method, 
-    //  the Dog class is still abstract, and as such cannot be instantiated
+    // This is what makes a functor a functor -- operator() can take a variable
+    //  number of arguments, and can return whatever we want 
+    bool operator()(std::string& text) {
+        return text == "lion";
+    }
 };
 
-class Labrador : public Dog 
+// Test is abstract, but we can still have pointers or references to a Test object
+void check(std::string text, Test& test)
 {
-public:
-    virtual void run() override { cout << "Running!" << endl; }
-};
+    if (test(text)) {
+        cout << "Text matches!" << endl;
+    } else {
+        cout << "No match" << endl;
+    }
+} 
 
 int main()
 {
-    Labrador d;
-    d.speak();  // From Dog class
-    d.run();    // From Labrador class
-    cout << endl;
+    MatchTest m;
+    std::string str = "lions";
 
-    // Cannot do this, because Animal class is abstract and this line will 
-    //  cause the Animal ctor to run 5 times...which cannot happen as Animal
-    //  is an abstract class. 
-    //      Animal animals[5];
-
-    // We can however create an array of Animal pointers, however when we loaded
-    //  the array we'd need to use concrete (ie; non-virtual) derived class objs
-    Animal* animals[5];
-    animals[0] = &d;
-    animals[0]->speak();
-    animals[0]->run();
+    // Now call check() and pass it our string and our functor reference
+    check(str, m);
 
     return 0;
 }
