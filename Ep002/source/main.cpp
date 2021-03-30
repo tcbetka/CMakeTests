cd200947e3dc62a099f314e6b8fbad7e6ce5c421
Tom Betka <tcbetka@hotmail.com>
2021-03-30 13:24:49 -0500

    Implemented function template example

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -2,6 +2,7 @@
 using std::cout;
 using std::endl;
 
+// Can have class templates
 template<typename T, typename U>
 class Test
 {
@@ -17,6 +18,12 @@ public:
     }
 };
 
+// Can also have function templates
+template<typename T>
+void print(T arg) {
+    cout << "arg: " << arg << endl;
+}
+
 
 int main()
 {
@@ -28,6 +35,14 @@ int main()
 
     Test<std::string, std::string> test3("Hello", "World");
     test3.print();
+
+    // Use our template function, explicitly indicating the template type
+    print<int>(1);
+    print<double>(3.14);
+
+    // Or...the compiler will even infer the type for us
+    print(4.56);
+    print("Hello");
 
     return 0;
 }
