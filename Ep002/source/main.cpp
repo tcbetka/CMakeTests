91e1ec3b936a413bb442a1fbfe03732cc491b99d
Tom Betka <tcbetka@hotmail.com>
2021-03-31 08:13:38 -0500

    Added decltype and auto examples

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -7,7 +7,13 @@ using std::vector;
 
 #include <typeinfo>
 
-
+// Here's an example of the use of decltype: It negates the need to know
+//  the type of the returned value, and can also evaluate expressions.
+template <class S, class T>
+auto test(S value1, T value2) -> decltype(value1 + value2)
+{
+    return value1 + " " + value2;
+}
 
 int main()
 {
@@ -26,11 +32,19 @@ int main()
     // decltype is a C++11 construct that allows us to declare variables based upon that 
     //  of an existing type. This is useful when we need to create a variable but we don't 
     //  actually know the type--but we can get it from the type of other variables in our program.
+
     // For example: This will create number as an integer
     decltype(intNum) number;
     cout << typeid(number).name() << endl; // output: i
 
+    auto text1 = "Hello";                   // const char*
+    auto text2 = std::string("Hello");      // std::string
+    cout << typeid(text1).name() << endl;   // output: 
+    cout << typeid(text2).name() << endl;   // output: 
 
+    auto test_value = test(std::string("hello"), std::string("world"));
+    cout << test_value << endl;
+    cout << typeid(test_value).name() << endl;
 
     return 0;
 }
