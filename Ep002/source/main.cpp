5b3cb5fe4184b8e0ed2abdfe9af2d2e8fab6dc4b
Tom Betka <tcbetka@hotmail.com>
2021-04-03 19:06:13 -0500

    End of lesson 80 -- implemented a rough scaling algorithm

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -21,13 +21,25 @@ int main()
     const int HEIGHT = 600;
 
     Bitmap bitmap(WIDTH, HEIGHT);
-    bitmap.setPixel(WIDTH/2, HEIGHT/2, 255, 255, 255);
 
-    // for (int y = 0; y < HEIGHT; y++) {
-    //     for (int x = 0; x < WIDTH; x++) {
-    //         bitmap.setPixel(x, y, 255, 0, 0);
-    //     }
-    // }
+    double min{999999};
+    double max{-999999};
+
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            double xFractal = (x - WIDTH/2) * (2.0/WIDTH);
+            double yFractal = (y - HEIGHT/2) * (2.0/HEIGHT);
+
+            if (yFractal < min) {
+                min = yFractal;
+            }
+            if (yFractal > max ) {
+               max = yFractal;
+            }
+         }
+    }
+
+    cout << min << ", " << max << endl;
 
     bitmap.write(fileName);
 
