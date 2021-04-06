78bf342726c3f1730613f0dd5f982eaba08aa52a
Tom Betka <tcbetka@hotmail.com>
2021-04-06 18:41:00 -0500

    First commit on particle fire explosion project

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -1,29 +1,7 @@
 #include <iostream>
-using std::cout;
-using std::endl;
 
-#include "FractalCreator.h"
-#include "Zoom.h"
-
-static const std::string fileName = "test.bmp";
-static constexpr int WIDTH = 800;
-static constexpr int HEIGHT = 600;
-
-int main()
+int main() 
 {
-    FractalCreator fractalCreator(WIDTH, HEIGHT);
-
-    fractalCreator.addRange(0.0, RGB(0, 0, 0));
-    fractalCreator.addRange(0.3, RGB(255, 0, 0));
-    fractalCreator.addRange(0.5, RGB(255, 255, 0));
-    fractalCreator.addRange(1.0, RGB(255, 255, 255));    
-
-    fractalCreator.addZoom(Zoom(295, 202, 0.1));
-    fractalCreator.addZoom(Zoom(312, 304, 0.1));
-
-    fractalCreator.run(fileName);
-
-    cout << "Finished" << endl;
 
     return 0;
 }
