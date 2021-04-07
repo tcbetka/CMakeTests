7c9c0e8f4bfdcb0af5fa53349b32167089495610
Tom Betka <tcbetka@hotmail.com>
2021-04-06 20:12:45 -0500

    End of kesson 62: Stopped using cmake as it's wonky, and a simply build script is much less complicated

--- Ep002/source/main.cpp
+++ Ep002/source/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+using std::cout;
+using std::endl;
+
+#include <SDL.h>
 
 int main() 
 {
+    constexpr int SCREEN_WIDTH = 1280;
+    constexpr int SCREEN_HEIGHT = 800;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL Init failed" << std::endl;
+        return -1;
+    }
+
+    cout << "SDL succeeded!" << endl;
+
+    // SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED
+    SDL_Window* window = SDL_CreateWindow("Particle Explosion Simulator", 
+            400, 400, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == nullptr) {
+        cout << "Error: " << SDL_GetError();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event event;
+    while(!quit) 
+    {
+        // Update particles
+        // Draw particles
+        
+        // Check for messages/events
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+
+        }
+    }
+
+    // Destroy window and then exit
+    SDL_DestroyWindow(window);
+    SDL_Quit(); 
 
     return 0;
 }
